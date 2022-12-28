34fe95dbee1aaf7c562679ec4ba89b48166cf0c6
pequito193 <vitorspequito@gmail.com>
2022-12-28 12:00:09 +0000

    Fix header routing

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,23 +6,26 @@ import './../styles/header.css';
 function Header() {
 
     const { isLoggedIn, setIsLoggedIn } = useState(false);
-    const { headerMessage, setHeaderMessage } = useState(<Link to={'/log-in'} className='links'>Log In</Link>)
+    let headerLink;
+    let headerMessage;
 
-    useEffect(() => {
-        if(isLoggedIn) {
-            setHeaderMessage(<Link to={'/log-out'} className='links'>Log Out</Link>)
-        }
-    }, []);
+    if(isLoggedIn) {
+        headerMessage = 'Log Out';
+        headerLink = '/log-out';
+    } else {
+        headerMessage = 'Log In';
+        headerLink = '/log-in';
+    }
 
     return (
         <React.Fragment>
             <div className="header">
                 <div className="trademark">
                     <img className="logo" src={require('./../assets/logo.png')} alt="logo" />
-                    <p className="website-name">Blog</p>
+                    <Link to={'/'} className="website-name">Blog</Link>
                 </div>
                 <ul>
-                    <li className="links">{headerMessage}</li>
+                    <li className="links"><Link to={headerLink} className='link'>{headerMessage}</Link></li>
                     <li className="links">Settings</li>
                 </ul>
             </div>
---
+++ src/components/postList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import './../styles/postList.css';
+
+function PostList() {
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+
+export default PostList;
