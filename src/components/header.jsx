36b30d69f55263fa28cdd08de14b9d191bb92de9
pequito193 <vitorspequito@gmail.com>
2022-12-29 12:31:59 +0000

    Revamp header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,24 +5,29 @@ import './../styles/header.css';
 
 function Header() {
 
-    const { isLoggedIn, setIsLoggedIn } = useState(false);
-    let headerLink;
-    let headerMessage;
+    const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+    const [ headerMessage, setHeaderMessage ] = useState('');
+    const [ headerLink, setHeaderLink ] = useState('');
 
-    if(isLoggedIn) {
-        headerMessage = 'Log Out';
-        headerLink = '/log-out';
-    } else {
-        headerMessage = 'Log In';
-        headerLink = '/log-in';
-    }
+    useEffect(() => {
+        if(isLoggedIn) {
+            setHeaderMessage('Log Out');
+            setHeaderLink('/log-out');
+        } else {
+            setHeaderMessage('Log In');
+            setHeaderLink('/log-in');
+        }
+    })
 
     return (
         <React.Fragment>
             <div className="header">
-                <div className="trademark">
-                    <img className="logo" src={require('./../assets/logo.png')} alt="logo" />
-                    <Link to={'/'} className="website-name">Blog</Link>
+                <div className="left-header">
+                    <Link to={'/'} className="website-name"><img className="logo" src={require('./../assets/logo.png')} alt="logo" /></Link>
+                    <form /*onSubmit={}*/ className="search">
+                        <button className="search-submit" type="submit"><img className="search-logo" src={require('./../assets/search.png')} alt="search button" /></button>
+                        <input type="text" className="search-input" required={true} placeholder="Search..."/>
+                    </form>
                 </div>
                 <ul>
                     <li className="links"><Link to={headerLink} className='link'>{headerMessage}</Link></li>
