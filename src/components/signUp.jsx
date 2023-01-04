0ca2097585029fcc6a6e0dc98579c800ad23d011
pequito193 <vitorspequito@gmail.com>
2023-01-04 11:24:53 +0000

    Finish sign up page

--- src/components/signUp.jsx
+++ src/components/signUp.jsx
@@ -7,11 +7,13 @@ function SignUp() {
     return(
         <React.Fragment>
             <h1 className="signup-title">Sign Up</h1>
-            <form className="signup-form" method='post' action="/users/login">
+            <form className="signup-form" method='post' action="/users/signup">
                 <input className="input" name="username" type="text" placeholder="Username" required={true}/>
+                <input className="input" name="email" type="email" placeholder="Email" required={true}/>
                 <input className="input" name="password" type="password" placeholder="Password" required={true}/>
+                <input className="input" name="confirmPassword" type="password" placeholder="Confirm Password" required={true}/>
                 <div className="no-account">
-                    <p>Don't have an account yet?
+                    <p>Already have an account?
                         <Link to={'/users/login'} className='login-link'> Log In </Link>
                     </p>
                 </div>
