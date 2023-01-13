a2e95f0c58db45251bfb733e95547d996fc9cf04
pequito193 <vitorspequito@gmail.com>
2023-01-13 13:08:17 +0000

    Added new post functionality

--- src/components/newPost.jsx
+++ src/components/newPost.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './../styles/newPost.css';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function NewPost() {
+function NewPost(props) {
+
+    const { JWT } = props;
+
+    const navigate = useNavigate();
+
+    function newPost(e) {
+        e.preventDefault();
+        try {
+            axios.post('/posts/new', {
+                title: e.target[0].value,
+                content: e.target[1].value,
+                jwt: JWT
+            })
+            .then(response => {
+                if (response.data.result === 'Success') {
+                    navigate('/');
+                }
+            })
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
 
     return (
         <React.Fragment>
             <h1 className="new-post-title">New Post</h1>
-            <form className="new-post" method="post" action="/posts">
+            <form className="new-post" method="post" onSubmit={newPost}>
                 <input name='title' className="input" type="text" placeholder="Title" maxLength={50} required={true}/>
                 <textarea name='post' className="input" type='text' rows={16} placeholder="Write your post here..." required={true}/>
                 <div className="wrapper">
