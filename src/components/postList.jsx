abd3b0e4021d5702bda9087647d460639f9d567b
pequito193 <vitorspequito@gmail.com>
2023-01-17 14:01:37 +0000

    Update component

--- src/components/postList.jsx
+++ src/components/postList.jsx
@@ -1,35 +1,43 @@
 import axios from "axios";
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import './../styles/postList.css';
 
 function PostList() {
 
-    const posts = [];
+    const [ posts, setPosts ] = useState([])
+
+    let dateFormat = {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        hour12: false
+    }
 
     useEffect(() => {
         axios.get('/posts/')
         .then(response => {
-            for (let i = 0; i < response.data.data.length; i++) {
-                posts.push(response.data.data[i])
-            }
-            console.log(posts)
+            setPosts(response.data.data)
         })
     }, []);
    
     return (
         <React.Fragment>
-            {posts.map((post, i) => {
-                console.log(post)
-                return(
-                    <div className='post' key={i}>
-                        <p className="post-title">{post.title}</p>
-                        <p className="post-content">{post.content}</p>
-                        <p className="post-date">{post.date}</p>
-                        <p className="post-likes">{post.likes}</p>
-                    </div>
-                )
-            })}
+            <div className="post-wrapper">
+                {posts.map((post, i) => {
+                    return(
+                        <div className='post' key={post.id}>
+                            <p className="post-title">{post.title}</p>
+                            <p className="post-content">{post.content}</p>
+                            <div className="post-info-wrapper">
+                                <p className="post-likes">{post.likes}</p>
+                                <p className="post-date">{post.date.toLocaleString('en-US', dateFormat)}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </React.Fragment>
     );
 }
